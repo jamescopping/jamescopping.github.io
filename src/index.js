20c8b5bbcbe4a360ecebc300acf6087335650e66
jamescopping <james.copping97@gmail.com>
2020-07-31 19:27:08 +0100

    simple

--- src/index.js
+++ src/index.js
@@ -5,16 +5,12 @@ import printMe from './print.js';
 function component() {
     const element = document.createElement('div');
 
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'world'], ' ');
     const btn = document.createElement('button');
 
-    btn.innerHTML = 'Click me and check the console!';
+    btn.innerHTML = 'Click me!';
     btn.onclick = printMe;
 
     element.appendChild(btn);
-
 
     return element;
 }
