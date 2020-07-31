2e589e57dcefa91c0ca300caabb7ffe74e5c9d8f
jamescopping <james.copping97@gmail.com>
2020-07-31 13:58:17 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import _ from 'lodash';
 import './style.css';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
 
+
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'world'], ' ');
-    element.classList.add('hello');
+    const btn = document.createElement('button');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+
     return element;
 }
 
