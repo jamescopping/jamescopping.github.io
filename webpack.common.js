781a3d27f20fb3447f57612cb7e0688f47794cc8
jamescopping <james.copping97@gmail.com>
2020-08-28 13:50:01 +0100

    anylitics

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
 
 module.exports = {
     entry: {
@@ -11,6 +12,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'Project-Pages',
         }),
+        new HtmlWebpackPartialsPlugin({
+            path: './src/partials/analytics.html',
+            location: 'head',
+            priority: 'high'
+        })
     ],
     output: {
         filename: '[name].bundle.js',
